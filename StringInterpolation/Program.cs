0881b96dc9aa00819c1e32bef42a96d592741be5
Mr-Abhishekk <munavalliabhishek1@gmail.com>
2026-02-09 21:43:38 +0530

    Add project files.

---
+++ StringInterpolation/Program.cs
@@ -0,0 +1,6 @@
+﻿Console.WriteLine("Enter Your First name:");
+string FName = Console.ReadLine();
+Console.WriteLine("Enter you Last Name:");
+string LName = Console.ReadLine();
+Console.WriteLine("Enter your Age:");
+int age=Convert.to
