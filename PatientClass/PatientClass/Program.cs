d7155d26800dd65a03f7ffebbef7e7a37bb98908
Mr-Abhishekk <munavalliabhishek1@gmail.com>
2026-02-09 22:16:28 +0530

    commit

---
+++ PatientClass/PatientClass/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace MyConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        } 
+    }
+         public class Patient
+        {
+            public string name = "John The Don";
+            public int age = 25;
+            public string gender = "Male";
+            public string BG = "O +ve";
+            public string loc="Bengaluru";
+            public string disease="Dengue";
+        }
+}
