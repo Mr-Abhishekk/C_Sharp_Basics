b54fde28a4213534e7e3383e4b71737640707276
Mr-Abhishekk <munavalliabhishek1@gmail.com>
2026-02-09 21:48:16 +0530

    commit

--- StringInterpolation/Program.cs
+++ StringInterpolation/Program.cs
@@ -3,4 +3,6 @@ string FName = Console.ReadLine();
 Console.WriteLine("Enter you Last Name:");
 string LName = Console.ReadLine();
 Console.WriteLine("Enter your Age:");
-int age=Convert.to
+int age = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine($"My name is {FName} {LName} and i am {age} years old");
+Console.ReadLine();
