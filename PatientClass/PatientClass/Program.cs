72ec3985bc95ee5f1cd88994523005652170279a
Mr-Abhishekk <munavalliabhishek1@gmail.com>
2026-02-09 22:16:28 +0530

    commit

---
+++ PatientClass/PatientClass/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace MyConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+
+        {
+            Patient pl = new Patient();
+            Console.WriteLine($"Patient Name is :{pl.name}");
+            Console.WriteLine($"Patient Age is :{pl.age}");
+            Console.WriteLine($"Patient Gender is:{pl.gender}");
+            Console.WriteLine($"Patient blood Group is :{pl.BG}");
+            Console.WriteLine($"Patient LOcation is :{pl.loc}");
+            Console.WriteLine($"Patient Disease is :{pl.disease}");
+        } 
+    }
+         public class Patient
+        {
+            public string name = "John The Don";
+            public int age = 25;
+            public string gender = "Male";
+            public string BG = "O +ve";
+            public string loc="Bengaluru";
+            public string disease="Dengue";
+        }
+}
