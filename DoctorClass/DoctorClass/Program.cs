dd018cacf6a0f1e574f390eeed2f9f28078f8b45
Mr-Abhishekk <munavalliabhishek1@gmail.com>
2026-02-10 21:53:22 +0530

    commit

---
+++ DoctorClass/DoctorClass/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace MyConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Doctor dr = new Doctor();
+            Console.WriteLine($"Doctor name is: {dr.name}");
+            Console.WriteLine($"Doctor is {dr.age} yeras old");
+            Console.WriteLine($"Doctor has {dr.exp} years of experience");
+            Console.WriteLine($"Doctor is a {dr.spl}");
+        }
+    }
+    class Doctor
+    {
+        public string name = "Dr Suresh";
+        public int age = 26;
+        public int exp = 4;
+        public string spl="Cardiologist";
+    }
+}
+
