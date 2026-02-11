418fb101ef80ef94772547d9e7f62e8b869e20cc
Mr-Abhishekk <munavalliabhishek1@gmail.com>
2026-02-11 21:46:04 +0530

    initial commit

---
+++ ModifyingObjectData/ModifyingObjectData/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace MyConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            doctor dl = new doctor();
+            Console.WriteLine(dl.name);
+            Console.WriteLine(dl.age);
+            Console.WriteLine(dl.exp);
+            Console.WriteLine(dl.spl);
+            dl.name = "Dr.Suresh";
+            dl.age = 30;
+            dl.exp = 8;
+            dl.spl = "Neurology";
+            Console.WriteLine(dl.name);
+            Console.WriteLine(dl.age);
+            Console.WriteLine(dl.exp);
+            Console.WriteLine(dl.spl);
+            Console.ReadLine();
+
+        }
+    }
+    public class doctor
+    {
+        public string name = "Dr Abhishek";
+        public int age = 22;
+        public int exp = 2;
+        public string spl = "Cardiology";
+    }
+}
+
